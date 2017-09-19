17a08c3db3ce7967a7cd1eb6aa8475f2bab69bff
Ram Sateesh Talari <ramsateesh@gmail.com>
2017-09-19 17:36:45 +0530

    Styling, Login, Signup and Blog Posts updated

--- blog.py
+++ blog.py
@@ -16,6 +16,18 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
 
 
+def handle_404(request, response, exception):
+    logging.exception(exception)
+    response.write('')
+    response.set_status(404)
+
+
+def handle_500(request, response, exception):
+    logging.exception(exception)
+    response.write('A server error occurred!')
+    response.set_status(500)
+
+
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
@@ -26,6 +38,20 @@ class Handler(webapp2.RequestHandler):
 
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
+
+    def handle_exception(self, exception, debug):
+        # Log the error.
+        logging.exception(exception)
+
+        # Set a custom message.
+        self.response.write('An error occurred.')
+
+        # If the exception is a HTTPException, use its error code.
+        # Otherwise use a generic 500 error code.
+        if isinstance(exception, webapp2.HTTPException):
+            self.response.set_status(exception.code)
+        else:
+            self.response.set_status(500)
 
 
 class UserHandler(Handler):
@@ -46,34 +72,132 @@ class UserHandler(Handler):
     def initialize(self, *a, **kw):
         webapp2.RequestHandler.initialize(self, *a, **kw)
         user_id = self.read_cookie(UserHandler.USER_COOKIE_KEY)
-        self.user = models.User.by_id(user_id)
+        self.user = None
+        if user_id is not None:
+            self.user = models.User.by_id(user_id)
+            if self.user is None:
+                self.redirect("/login")
 
-class WelcomeHandler(UserHandler):
+
+class BlogHandler(UserHandler):
+    def render_blogs(self, blogs = []):
+        self.render("index.html",
+                    username = self.user.name,
+                    blog_entries = blogs)
+ 
     def get(self):
         if self.user is None:
-            self.redirect("/signup")
+            self.redirect("/login")
         else:
-            self.render("welcome.html", username = self.user.name)
+            blogs = models.Blog.by_user_id(self.user.key.id())
+            self.render_blogs(blogs = blogs)
 
 
-class HomePageHandler(UserHandler):
+class BlogPostHandler(UserHandler):
     def get(self):
+       if self.user is None:
+            self.redirect("/login")
+       self.render("newpost.html", blog_entry = models.Blog())
+
+    def isValidData(self, title, text):
+        if title is None or title == "":
+            return False
+        return True
+
+    def post(self):
         if self.user is None:
-            self.redirect("/signup")
+            self.redirect("/login")
+        title = self.request.get('subject')
+        blog_text = self.request.get('content')
+        blog_entry = models.Blog()
+        if self.isValidData(title, blog_text):
+            blog_entry.title = title
+            blog_entry.content = blog_text
+            blog_entry.user_id = self.user.key.id()
+            blog_entry = blog_entry.put().get()
+            self.redirect("/blog/%d" % blog_entry.key.id())
         else:
-            self.redirect("/welcome")
+            self.render_newpost(blog_entry, "You need a valid title and blog content.")
+
+
+class BlogPageHandler(BlogHandler):
+    def get(self, blog_id):
+        if self.user is None:
+            self.redirect("/login")
+
+        blog = models.Blog.by_id(blog_id)
+        comments = models.Comment.by_blog_id(blog_id)
+        self.render("blog_entry.html", blog_entry = blog,
+                    comments = comments, user = self.user)
+
+
+class LoginHandler(UserHandler):
+    def render_login(self,
+                     valid_username=True,
+                     valid_password = True,
+                     invalid_credentials = False,
+                     form_validated = False,
+                     user_not_found = False):
+        self.render("login.html",
+                    user_not_found = user_not_found,
+                    valid_username = valid_username,
+                    valid_password = valid_password,
+                    invalid_credentials = invalid_credentials,
+                    form_validated = form_validated)
+
+    def get(self):
+        self.render_login()
+
+    def post(self):
+        username = self.request.get("username")
+        password = self.request.get("password")
+
+        valid_username = utils.valid_username(username)
+        valid_password = utils.valid_password(password)
+
+        user = models.User.by_name(username)
+
+        if user is None:
+            self.render_login(user_not_found = True,
+                              form_validated = True)
+        elif utils.valid_pw(username, password, user.pw_hash):
+            self.login(user)
+            self.redirect("/")
+        else:
+            self.render_login(valid_username = valid_username,
+                              valid_password = valid_password,
+                              invalid_credentials = True,
+                              form_validated = True)
 
 class LogoutHandler(UserHandler):
     def get(self):
         if self.user is not None:
             self.logout()
-        self.redirect("/signup")
+        self.redirect("/login")
 
 
 class SignupHandler(UserHandler):
     SIGNUP_PAGE = "signup.html"
+
+    def render_signup(self, valid_username=False,
+                      matching_passwords = False,
+                      valid_password = False,
+                      valid_email = False,
+                      username = None,
+                      email = None):
+        self.render(SignupHandler.SIGNUP_PAGE,
+                    valid_username = valid_username,
+                    matching_passwords = matching_passwords,
+                    valid_password = valid_password,
+                    valid_email = valid_email,
+                    username = username,
+                    email = email)
+
     def get(self):
-        self.render(SignupHandler.SIGNUP_PAGE)
+        if self.user is not None:
+            self.redirect("/")
+        else:
+            self.render_signup()
 
     def post(self):
         username = self.request.get("username")
@@ -97,20 +221,24 @@ class SignupHandler(UserHandler):
 
            self.login(usr)
 
-           self.redirect('/welcome')
+           self.redirect('/')
         else:
-            self.render(SignupHandler.SIGNUP_PAGE,
-                        valid_username = valid_username,
-                        valid_password = valid_password,
-                        matching_passwords = matching_passwords,
-                        valid_email = valid_email,
-                        username = username,
-                        email = email)
+            self.render_signup(valid_username = valid_username,
+                               valid_password = valid_password,
+                               matching_passwords = matching_passwords,
+                               valid_email = valid_email,
+                               username = username,
+                               email = email)
 
 
 app = webapp2.WSGIApplication([
-    ('/', HomePageHandler),
+    ('/', BlogHandler),
+    ('/login', LoginHandler),
     ('/signup', SignupHandler),
-    ('/welcome', WelcomeHandler),
     ('/logout', LogoutHandler),
+    ('/newpost', BlogPostHandler),
+    ('/blog/(\d+)', BlogPageHandler),
 ], debug=True)
+
+app.error_handlers[404] = handle_404
+app.error_handlers[500] = handle_500
--- models/__init__.py
+++ models/__init__.py
@@ -20,7 +20,7 @@ class User(db.Model):
 
     @classmethod
     def by_name(cls, name):
-        u = User.query().filter('name =', name).fetch()
+        u = User.query(User.name == name).get()
         return u
 
 
@@ -37,3 +37,52 @@ class User(db.Model):
         u = cls.by_name(name)
         if u and utils.valid_pw(name, pw, u.pw_hash):
             return u
+
+
+class Blog(db.Model):
+     title = db.StringProperty(required = True)
+     content = db.TextProperty()
+     user_id = db.IntegerProperty(required = True)
+     created = db.DateTimeProperty(auto_now_add=True)
+
+
+     @classmethod
+     def by_id(cls, uid):
+         if uid is None:
+             return None
+         uid = long(uid)
+         return Blog.get_by_id(uid)
+
+     @classmethod
+     def by_user_id(cls, user_id):
+         if user_id is None:
+             return None
+         return Blog.query(Blog.user_id == user_id).fetch()
+
+
+class Like(db.Model):
+    blog_id = db.IntegerProperty(required = True)
+    user_id = db.IntegerProperty(required = True)
+    like = db.BooleanProperty()
+
+    @classmethod
+    def by_blog_id_and_user_id(cls, user_id, blog_id):
+        if user_id is None or blog_id is None:
+            return None
+        else:
+            return Like.query(ndb.AND(Like.user_id == user_id, Like.blog_id == blog_id)).get()
+
+
+class Comment(db.Model):
+    blog_id = db.IntegerProperty(required = True)
+    user_id = db.IntegerProperty(required = True)
+    username = db.StringProperty(required = True)
+    comment_text = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
+    @classmethod
+    def by_blog_id(cls, blog_id):
+        if blog_id is None:
+            return None
+
+        return Comment.query(Comment.blog_id == long(blog_id)).fetch()
